c839f34f3de61c0d579b50febff237b1355c120c
evil-coconut <richard.morgan2951@yandex.ru>
2023-08-11 19:28:04 +0700

    Updates

---
+++ js/index.1.4c70439d.js
@@ -0,0 +1,1 @@
+new Swiper(".swiper",{direction:"horizontal",pagination:{el:".swiper-pagination",clickable:!0},slidesPerView:6,watchOverflow:!0,spaceBetween:30,breakpoints:{320:{slidesPerView:1},600:{slidesPerView:2},915:{slidesPerView:3},1230:{slidesPerView:4},1545:{slidesPerView:5},1860:{slidesPerView:6}}});
